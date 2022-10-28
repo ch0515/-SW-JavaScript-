6d72470e2a21afeb75d9fb4a324c9a18ed103eff
chaemin <w2114@e-mirim.hs.kr>
2022-10-29 01:37:17 +0900

    비교연산자

---
+++ 02/Training6.js
@@ -0,0 +1,10 @@
+document.writeln(10 == 10);
+document.writeln(10 === 10);
+document.writeln(10 == "10");
+document.writeln(10 === "10");
+document.writeln(10 !== 20);
+document.writeln(10 > 20);
+document.writeln(10 >= 20);
+document.writeln(10 < 20);
+document.writeln(10 <= 20);
+//writeln 은 write이거랑 같은 기능을 하는데 별로 사용하는 걸 권장하지 않음
